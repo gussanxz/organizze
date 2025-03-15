6fedf1ea55712e84ec13ac250ad47a23e3087b15
gustavo <italodta@gmail.com>
2025-03-15 15:08:08 -0400

    add lib material-intro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
 
     // Add the dependencies for any other desired Firebase products
     // https://firebase.google.com/docs/android/setup#available-libraries
+
+    implementation("com.github.heinrichreimer:material-intro:2.0.0")
 }
