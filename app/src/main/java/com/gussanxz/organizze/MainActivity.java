3903aecd91f851b52481541c4d46b6a1f7db97d3
gustavo <italodta@gmail.com>
2025-03-16 19:49:32 -0400

    criado slider c/ SimpleSlide

--- app/src/main/java/com/gussanxz/organizze/MainActivity.java
+++ app/src/main/java/com/gussanxz/organizze/MainActivity.java
@@ -7,12 +7,41 @@ import android.view.View;
 
 import com.gussanxz.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descricao")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descricao2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descricao3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
     }
 }
