a5e0906a156c51f5aaf188153e9e181d399b99cf
gustavo <italodta@gmail.com>
2025-03-16 19:50:26 -0400

    criado slider com FragmentSlide

--- app/src/main/java/com/gussanxz/organizze/MainActivity.java
+++ app/src/main/java/com/gussanxz/organizze/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import com.gussanxz.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
@@ -16,6 +17,21 @@ public class MainActivity extends IntroActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        /*
         setButtonBackVisible(false);
         setButtonNextVisible(false);
 
@@ -42,6 +58,6 @@ public class MainActivity extends IntroActivity {
                 .background(android.R.color.holo_orange_light)
                 .build()
         );
-
+         */
     }
 }
