bb0aa26e9b2d459943145cd306776e317853b457
gustavo <italodta@gmail.com>
2025-03-16 21:10:19 -0400

    adicionado slider com tela de login e cadastro

--- app/src/main/java/com/gussanxz/organizze/MainActivity.java
+++ app/src/main/java/com/gussanxz/organizze/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 
+import com.gussanxz.organizze.activity.CadastroActivity;
 import com.gussanxz.organizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
@@ -38,8 +39,22 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
                 .canGoForward(false)
                 .build()
         );
     }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
 }
