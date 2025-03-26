53ce84dc3d28ed8897644cf70bab481b091f1951
gustavo <italodta@gmail.com>
2025-03-26 06:44:18 -0400

    ids despesas instanciados e inserido data atual em despesas activity

--- app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
@@ -1,17 +1,31 @@
 package com.gussanxz.organizze.activity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.material.textfield.TextInputEditText;
 import com.gussanxz.organizze.R;
+import com.gussanxz.organizze.helper.DateCustom;
 
 public class DespesasActivity extends AppCompatActivity {
+
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_despesas);
+
+        campoData = findViewById(R.id.editData);
+        campoCategoria = findViewById(R.id.editCategoria);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+
+        //Prenche o campo data com a data atual
+        campoData.setText( DateCustom.dataAtual() );
     }
 }
