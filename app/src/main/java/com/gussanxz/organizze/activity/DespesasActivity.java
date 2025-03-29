1a141f87f38ceb5d89e036a1860284f30f3d6616
gustavo <italodta@gmail.com>
2025-03-29 13:04:34 -0400

    salvando despesas no database

--- app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
@@ -1,6 +1,7 @@
 package com.gussanxz.organizze.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -8,11 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.gussanxz.organizze.R;
 import com.gussanxz.organizze.helper.DateCustom;
+import com.gussanxz.organizze.model.Movimentacao;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,19 @@ public class DespesasActivity extends AppCompatActivity {
 
         //Prenche o campo data com a data atual
         campoData.setText( DateCustom.dataAtual() );
+    }
+
+    public void salvarDespesa(View view) {
+
+    String data = campoData.getText().toString();
+    movimentacao = new Movimentacao();
+    movimentacao.setValor( Double.parseDouble(campoValor.getText().toString()) );
+    movimentacao.setCategoria( campoCategoria.getText().toString() );
+    movimentacao.setDescricao( campoDescricao.getText().toString() );
+    movimentacao.setData( data );
+    movimentacao.setTipo( "d" );
+
+    movimentacao.salvar( data );
+
     }
 }
--- app/src/main/java/com/gussanxz/organizze/helper/DateCustom.java
+++ app/src/main/java/com/gussanxz/organizze/helper/DateCustom.java
@@ -1,5 +1,7 @@
 package com.gussanxz.organizze.helper;
 
+import static android.content.ContentValues.TAG;
+import android.util.Log;
 import java.text.SimpleDateFormat;
 
 public class DateCustom {
@@ -13,4 +15,15 @@ public class DateCustom {
 
     }
 
+    public static String mesAnoDataEscolhida( String data ) {
+
+        String retornoData[] = data.split("/"); //ex: 23/01/2018
+        String dia = retornoData[0]; //dia 23
+        String mes = retornoData[1]; //mes 01
+        String ano = retornoData[2]; //ano 2018
+        Log.i(TAG, "Data informada: " + mes + "/" + ano );
+        String mesAno = mes + ano;
+        return  mesAno;
+
+    }
 }
