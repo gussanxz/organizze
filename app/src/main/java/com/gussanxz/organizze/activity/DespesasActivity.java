e7a64ea5a37c58dcb936649fa3f42c44ac70f449
gustavo <italodta@gmail.com>
2025-03-29 16:50:33 -0400

    validação de campos em Despesas

--- app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
@@ -3,9 +3,11 @@ package com.gussanxz.organizze.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.textfield.TextInputEditText;
 import com.gussanxz.organizze.R;
 import com.gussanxz.organizze.helper.DateCustom;
@@ -29,20 +31,66 @@ public class DespesasActivity extends AppCompatActivity {
         campoValor = findViewById(R.id.editValor);
 
         //Prenche o campo data com a data atual
-        campoData.setText( DateCustom.dataAtual() );
+        campoData.setText(DateCustom.dataAtual());
     }
 
     public void salvarDespesa(View view) {
 
-    String data = campoData.getText().toString();
-    movimentacao = new Movimentacao();
-    movimentacao.setValor( Double.parseDouble(campoValor.getText().toString()) );
-    movimentacao.setCategoria( campoCategoria.getText().toString() );
-    movimentacao.setDescricao( campoDescricao.getText().toString() );
-    movimentacao.setData( data );
-    movimentacao.setTipo( "d" );
+        if (validarCamposDespesas()) {
 
-    movimentacao.salvar( data );
+            String data = campoData.getText().toString();
+            movimentacao = new Movimentacao();
+            movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+            movimentacao.setCategoria(campoCategoria.getText().toString());
+            movimentacao.setDescricao(campoDescricao.getText().toString());
+            movimentacao.setData(data);
+            movimentacao.setTipo("d");
+
+            movimentacao.salvar(data);
+
+        }
+    }
+
+
+    public Boolean validarCamposDespesas() {
+
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescricao = campoDescricao.getText().toString();
+
+        if ( !textoValor.isEmpty()) {
+            if ( !textoData.isEmpty()) {
+                if ( !textoCategoria.isEmpty()) {
+                    if ( !textoDescricao.isEmpty()) {
+
+                        return true;
+
+                    }else {
+                        Toast.makeText(DespesasActivity.this,
+                                "Descrição não foi preenchida!", Toast.LENGTH_SHORT).show();
+                        return false;
+
+                    }
+                }else {
+                    Toast.makeText(DespesasActivity.this,
+                            "Categoria não foi preenchida!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }else {
+                Toast.makeText(DespesasActivity.this,
+                        "Data não foi preenchida!", Toast.LENGTH_SHORT).show();
+                return false;
+
+            }
+
+        }else {
+            Toast.makeText(DespesasActivity.this,
+                    "Valor não foi preenchido!", Toast.LENGTH_SHORT).show();
+            return false;
+
+        }
 
     }
+
 }
