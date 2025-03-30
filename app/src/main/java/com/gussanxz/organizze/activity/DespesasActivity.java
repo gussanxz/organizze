93e528ba98e4dd28519aa00d3739750e62132635
gustavo <italodta@gmail.com>
2025-03-30 08:28:03 -0400

    adicionando despesaTotal

--- app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/gussanxz/organizze/activity/DespesasActivity.java
@@ -9,48 +9,64 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.gussanxz.organizze.R;
+import com.gussanxz.organizze.config.ConfiguracaoFirebase;
+import com.gussanxz.organizze.helper.Base64Custom;
 import com.gussanxz.organizze.helper.DateCustom;
 import com.gussanxz.organizze.model.Movimentacao;
+import com.gussanxz.organizze.model.Usuario;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
     private Movimentacao movimentacao;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private Double despesaTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_despesas);
 
+        campoValor = findViewById(R.id.editValor);
         campoData = findViewById(R.id.editData);
         campoCategoria = findViewById(R.id.editCategoria);
         campoDescricao = findViewById(R.id.editDescricao);
-        campoValor = findViewById(R.id.editValor);
 
         //Prenche o campo data com a data atual
         campoData.setText(DateCustom.dataAtual());
+        recuperarDespesaTotal();
+
     }
 
     public void salvarDespesa(View view) {
 
         if (validarCamposDespesas()) {
 
-            String data = campoData.getText().toString();
             movimentacao = new Movimentacao();
-            movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+            String data = campoData.getText().toString();
+            Double valorRecuperado = Double.parseDouble(campoValor.getText().toString());
+
+            movimentacao.setValor( valorRecuperado );
             movimentacao.setCategoria(campoCategoria.getText().toString());
             movimentacao.setDescricao(campoDescricao.getText().toString());
             movimentacao.setData(data);
             movimentacao.setTipo("d");
 
+            Double despesaAtualizada = despesaTotal + valorRecuperado;
+            atualizarDespesa( despesaAtualizada );
+
             movimentacao.salvar(data);
 
         }
     }
-
 
     public Boolean validarCamposDespesas() {
 
@@ -90,6 +106,37 @@ public class DespesasActivity extends AppCompatActivity {
             return false;
 
         }
+
+    }
+
+    public void recuperarDespesaTotal() {
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child( idUsuario );
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue( Usuario.class );
+                despesaTotal = usuario.getDespesaTotal();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void atualizarDespesa(Double despesa) {
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.child("despesaTotal").setValue(despesa);
 
     }
 
