477a6b10b1137d4d72fb25eafa410ee8f9726d04
gustavo <italodta@gmail.com>
2025-04-26 22:03:00 -0400

    criando CadastroCliente

---
+++ app/src/main/java/com/gussanxz/organizze/activity/CadastroClienteActivity.java
@@ -0,0 +1,18 @@
+package com.gussanxz.organizze.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gussanxz.organizze.R;
+
+public class CadastroClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro_cliente);
+    }
+}
