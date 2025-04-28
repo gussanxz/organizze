042460aa0fdf8cdd650f22d708259fccafd02fbb
gustavo italo <gussanxz@gmail.com>
2025-04-28 02:31:34 -0400

    replicando mudancas de despesas em proventos

--- app/src/main/java/com/gussanxz/organizze/activity/ProventosActivity.java
+++ app/src/main/java/com/gussanxz/organizze/activity/ProventosActivity.java
@@ -1,5 +1,6 @@
 package com.gussanxz.organizze.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -67,6 +68,10 @@ public class ProventosActivity extends AppCompatActivity {
 
             finish();
         }
+    }
+
+    public void retornarPrincipal(View view){
+        startActivity(new Intent(this, PrincipalActivity.class));
     }
 
     public Boolean validarCamposProventos() {
