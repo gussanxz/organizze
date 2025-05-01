95fb252a3d8b68fb981f44b38e08fbd09577eb36
gustavo italo <gussanxz@gmail.com>
2025-05-01 14:30:37 -0400

    criando nova tela principal

---
+++ app/src/main/java/com/gussanxz/organizze/activity/HomeActivity.java
@@ -0,0 +1,36 @@
+package com.gussanxz.organizze.activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.gussanxz.organizze.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView textoContas, textoVendas, textoMercado, textoAtividades, textoConfigs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textoContas = findViewById(R.id.textViewContas);
+        textoVendas = findViewById(R.id.textViewVendas);
+        textoMercado = findViewById(R.id.textViewMercado);
+        textoAtividades = findViewById(R.id.textViewAtividades);
+        textoConfigs = findViewById(R.id.textViewConfigs);
+
+    }
+}
