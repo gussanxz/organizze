f5bb4fea006744fbf496779e492ecf09667d5494
gustavo italo <gussanxz@gmail.com>
2025-05-02 22:16:15 -0400

    Iniciando ResumoVendasActivity

--- app/src/main/java/com/gussanxz/organizze/activity/ResumoVendasActivity.java
+++ app/src/main/java/com/gussanxz/organizze/activity/ResumoVendasActivity.java
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
 
 import com.gussanxz.organizze.R;
 
 public class ResumoVendasActivity extends AppCompatActivity {
+    private final String TAG = "ResumoVendasActivity";
+    private TextView textValorPendente, textVendasPendentes, textVendasPagas,
+            textDataVenda, textValorVenda, textValorPendenteRestante, textDataPagamento, textValorPagamento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,24 @@ public class ResumoVendasActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textValorPendente = findViewById(R.id.textValorPendente);
+        textVendasPendentes = findViewById(R.id.textVendasPendentes);
+        textVendasPagas = findViewById(R.id.textVendasPagas);
+        textDataVenda = findViewById(R.id.textDataVenda);
+        textValorVenda = findViewById(R.id.textValorVenda);
+        textValorPendenteRestante = findViewById(R.id.textValorPendenteRestante);
+        textDataPagamento = findViewById(R.id.textDataPagamento);
+        textValorPagamento = findViewById(R.id.textValorPagamento);
+
+        // Simula dados de teste para visualização
+        textValorPendente.setText("50,00");
+        textVendasPendentes.setText("1");
+        textVendasPagas.setText("0");
+        textDataVenda.setText("sábado, 08 fevereiro 2025");
+        textValorVenda.setText("R$ 100,00");
+        textValorPendenteRestante.setText("R$ 50,00");
+        textDataPagamento.setText("08/02/2025");
+        textValorPagamento.setText("R$ 50,00");
     }
 }
