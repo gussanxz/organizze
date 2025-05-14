bc68e1d23a0b0f520fc3c9976a16a6875fd22135
gustavo italo <gussanxz@gmail.com>
2025-05-13 22:51:25 -0400

    adicionando nó categorias

--- app/src/main/java/com/gussanxz/orgafacil/model/Usuario.java
+++ app/src/main/java/com/gussanxz/orgafacil/model/Usuario.java
@@ -1,8 +1,17 @@
 package com.gussanxz.orgafacil.model;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
+import com.google.firebase.database.ValueEventListener;
 import com.gussanxz.orgafacil.config.ConfiguracaoFirebase;
+
+import java.util.HashMap;
 
 public class Usuario {
 
@@ -18,9 +27,24 @@ public class Usuario {
 
     public void salvar() {
         DatabaseReference firebase = ConfiguracaoFirebase.getFirebaseDatabase();
-        firebase.child("usuarios")
-                .child(this.idUsuario)
-                .setValue(this);
+        DatabaseReference usuarioRef = firebase.child("usuarios").child(getIdUsuario());
+
+        usuarioRef.setValue(this);
+
+        usuarioRef.child("categorias").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    // Cria um mapa vazio (pode começar sem categorias)
+                    usuarioRef.child("categorias").setValue(new HashMap<>());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("Firebase", "Erro ao verificar categorias", error.toException());
+            }
+        });
     }
 
     public Double getProventosTotal() {
